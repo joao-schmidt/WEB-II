73fb7982d56f9aa6119c8d74d756543bc11e4363
joao-schmidt <127263929+joao-schmidt@users.noreply.github.com>
2024-03-22 20:19:43 -0300

    aula dia 22/03 verificar codigo
    update

--- aula1/alunos/models.py
+++ aula1/alunos/models.py
@@ -3,5 +3,19 @@ from django.db import models
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=200)
-    data_nascimento = models.DateTimeField("data punlished")
+    data_nascimento = models.DateTimeField()
 # Create your models here.
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nome
+    
+class Disciplina(models.Model):
+    #chave estrangeira para Curso.
+    curso= models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nome= models.CharField(max_length=200)
+    codigo = models.CharField(max_length=6, null=True)
+    
+
